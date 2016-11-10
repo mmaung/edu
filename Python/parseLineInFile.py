c9027a372585f7a840efeac6c2c8e88d867f3037
Max Maung <max@nod-labs.com>
2016-11-09 18:26:52 -0800

    Update Python line parsing script

--- Python/parseLineInFile.py
+++ Python/parseLineInFile.py
@@ -7,11 +7,16 @@ if len(sys.argv) != 2:
 	exit(-1);
 firstVal = True
 prevTime = 0.0
-largestDiffTime = -sys.maxsize - 1;
-smallestDiffTime = sys.maxsize;
+largestDiffTime = -sys.maxsize - 1
+largestDiffString = ""
+prevLargestDiffString = ""
+smallestDiffTime = sys.maxsize
+smallestDiffString = ""
+prevSmallestDiffString = ""
+prevLine = ""
 inFile = open(sys.argv[1], 'r')
 for line in inFile :
-	if "Zi:1:062:1" in line :
+	if "Bi:1:018:1 0 19" in line :
 		currentTime = int(line.split(" ")[1])
 		if firstVal :
 			firstVal = False
@@ -20,10 +25,18 @@ for line in inFile :
 			print diffTime
 			if diffTime > largestDiffTime :
 				largestDiffTime = diffTime
+				largestDiffString = line
+				prevLargestDiffString = prevLine
 			elif diffTime < smallestDiffTime :
 				smallestDiffTime = diffTime
+				smallestDiffString = line
+				prevSmallestDiffString = prevLine
 		prevTime = currentTime
+		prevLine = line
 
 print "largest diff time: " + repr(largestDiffTime)
 print "smallest diff time: " + repr(smallestDiffTime)
-
+print largestDiffString
+print prevLargestDiffString
+print smallestDiffString
+print prevSmallestDiffString
